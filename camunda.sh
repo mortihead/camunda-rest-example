b64fdee269621c9b50f56c4139dbff4943dcaac9
NBochkarev <NBochkarev@ibs.ru>
2023-07-24 19:31:38 +0500

    Add camunda.sh

---
+++ camunda.sh
@@ -0,0 +1,5 @@
+# x86
+# docker run -d --platform linux/amd64 --name camunda -p 8080:8080 camunda/camunda-bpm-platform:latest
+
+# Arm64 (Apple Silicone)
+docker run -d --platform linux/arm64 --name camunda -p 8080:8080 camunda/camunda-bpm-platform:latest
